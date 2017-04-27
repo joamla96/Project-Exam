a30b67e34f18ba73126825029229135c86c00f42
Jonas Laursen <jona8690@edu.eal.dk>
2017-04-27 14:02:21 +0200

    started

---
+++ RoomReservationSystem/Core.UnitTest/ClientCreateReservation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.UnitTest
+{
+	[TestClass]
+    public class ClientCreateReservation
+    {
+		[TestInitialize]
+
+    }
+}

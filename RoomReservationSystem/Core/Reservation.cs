95bb75f779f6c8621d4105d6413308f7463b6dfc
Roxana Ion <ionroxana95@yahoo.com>
2017-04-27 15:28:09 +0200

    a

--- RoomReservationSystem/Core/Reservation.cs
+++ RoomReservationSystem/Core/Reservation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Core
 {
     public class Reservation
     {
+        public IUser User { get; set; }
+        public IRoom Room { get; set; }
+        public int People { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
     }
 }

2d96d99cffa599937fca9d118f6106311116794d
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2017-04-27 15:28:08 +0200

    created variables and first method

---
+++ RoomReservationSystem/Core.UnitTest/CreateInstance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core.Interfaces;
+
+namespace Core.UnitTest
+{
+    [TestClass]
+    public class CreateInstance
+    {
+        IRoom _room1;
+        IRoom _room2;
+        IRoom _room3;
+
+        IUser _student;
+        IUser _teacher;
+        IUser _admin;
+
+        Reservation _reservation1;
+        Reservation _reservation2;
+        Reservation _reservation3;
+
+        [TestMethod]
+        public void CanCreateRoomInstance()
+        {
+            _room1 = new Room()
+        }
+    }
+}

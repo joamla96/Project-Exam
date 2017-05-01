e2cebeef02ff2cb1d04787b3ca94fda83b7b5c7f
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2017-05-01 09:06:18 +0200

    working on room repository, put everything in testinitialize for createinstance

--- RoomReservationSystem/Core.UnitTest/RoomRespositoryTests.cs
+++ RoomReservationSystem/Core.UnitTest/RoomRespositoryTests.cs
@@ -10,10 +10,14 @@ namespace Core.UnitTest
     {
         RoomRepository _repoRoom;
 
+        static IUser _student = new User("matt2694", "matt2694@edu.eal.dk", Permission.Student);
+        static IUser _teacher = new User("alhe", "alhe@eal.dk", Permission.Teacher);
+        static IUser _admin = new User("frje", "frje@eal.dk", Permission.Admin);
+
         [TestInitialize]
         public void TestInitialize()
         {
-
+            _repoRoom = new RoomRepository();
         }
     }
 }

2c372640cac38402812ef6c3ec07328e010d5639
Jonas Laursen <jona8690@edu.eal.dk>
2017-05-05 13:02:45 +0200

    setting up some integration tests

--- RoomReservationSystem/Core.IntegrationTest/RequestReservations.cs
+++ RoomReservationSystem/Core.IntegrationTest/RequestReservations.cs
@@ -3,9 +3,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Core;
 using Core.Interfaces;
 
-namespace Core.IntegrationTest
-{
-    public class RequestReservations
-    {
-    }
+namespace Core.IntegrationTest {
+	[TestClass]
+	public class RequestReservations {
+		ReservationRepository _repoReserv = ReservationRepository.Instance;
+		UserRepository _repoUser = UserRepository.Instance;
+		RoomRepository _repoRoom = RoomRepository.Instance;
+
+		IRoom _room1;
+
+		[TestInitialize]
+		public void Init() {
+			_repoReserv.Clear();
+			_repoUser.Clear();
+			_repoRoom.Clear();
+		}
+	}
 }

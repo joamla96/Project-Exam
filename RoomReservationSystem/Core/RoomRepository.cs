37a2f7aa04f80eebed0b7693c75b322c23b52376
Hedviga <hedv0147@edu.eal.dk>
2017-05-05 13:34:13 +0200

    gui

--- RoomReservationSystem/Core/RoomRepository.cs
+++ RoomReservationSystem/Core/RoomRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Interfaces;
+using Core.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace Core {
         //{
         //    throw new NotImplementedException();
         //}
+        
 
         public void Clear()
         {

ce2dd4bd690b3323f53a2855cdc8258c63f6245f
Matthew Peterson <matt2694@edu.eal.dk>
2017-05-11 13:05:24 +0200

    SP_

--- SQL/FinalStoredProcedures.sql
+++ SQL/FinalStoredProcedures.sql
@@ -1,16 +1,16 @@
-CREATE PROCEDURE GetAllUsers AS
+CREATE PROCEDURE SP_GetAllUsers AS
 BEGIN
 SELECT Username, Email, PermissionLevel
 FROM Users
 END
 
-CREATE PROCEDURE GetAllRooms AS
+CREATE PROCEDURE SP_GetAllRooms AS
 BEGIN
 SELECT Building, FloorNr, Nr, MaxPeople, MinPermissionLevel
 FROM Rooms
 END
 
-CREATE PROCEDURE GetAllReservations AS
+CREATE PROCEDURE SP_GetAllReservations AS
 BEGIN
 SELECT ID, PeopleNr, DateTo, DateFrom, Building, FloorNr, Nr, Username
 FROM Reservations

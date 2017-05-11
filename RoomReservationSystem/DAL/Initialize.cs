57f7094e46a7da270265bed81034ab9c02cfe4cf
Jonas Laursen <jona8690@edu.eal.dk>
2017-05-11 14:22:07 +0200

    started initi. functions

--- RoomReservationSystem/DAL/Initialize.cs
+++ RoomReservationSystem/DAL/Initialize.cs
@@ -3,6 +3,9 @@ using Core;
 
 namespace DAL {
 	public static class Initialize {
+		private static ReservationRepository _repoReserv = ReservationRepository.Instance;
+		private static RoomRepository _repoRoom = RoomRepository.Instance;
+		private static UserRepository _repoUser = UserRepository.Instance;
 		
 		public static void StartUp() {
 

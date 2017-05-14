c9acd29ae98e1077932932f0d7c86684b12df5ae
Matthew Peterson <matt2694@edu.eal.dk>
2017-05-14 16:51:12 +0200

    Insert Stored Procedures

--- SQL/FinalStoredProcedures.sql
+++ SQL/FinalStoredProcedures.sql
@@ -15,3 +15,34 @@ BEGIN
 SELECT ID, PeopleNr, DateTo, DateFrom, Building, FloorNr, Nr, Username
 FROM Reservations
 END
+
+CREATE PROCEDURE SP_InsertUser (@Username NVarChar(100),
+								@Email NVarChar(MAX),
+								@PermissionLevel Int) AS
+BEGIN
+INSERT INTO Users (Username, Email, PermissionLevel) VALUES
+				  (@Username, @Email, @PermissionLevel)
+END
+
+CREATE PROCEDURE SP_InsertRoom (@Building Char,
+								@FloorNr Int,
+								@Nr Int,
+								@MaxPeople Int,
+								@MinPermissionLevel Int) AS
+BEGIN
+INSERT INTO Rooms (Building, FloorNr, Nr, MaxPeople, MinPermissionLevel) VALUES
+				  (@Building, @FloorNr, @Nr, @MaxPeople, @MinPermissionLevel)
+END
+
+CREATE PROCEDURE SP_InsertReservation (@PeopleNr Int,
+								       @DateTo DateTime2,
+								       @DateFrom DateTime2,
+									   @Building Char,
+									   @FloorNr Int,
+									   @Nr Int,
+									   @Username NVarChar(100)) AS
+BEGIN
+INSERT INTO Reservations (PeopleNr, DateTo, DateFrom, Building, FloorNr, Nr, Username) VALUES
+				         (@PeopleNr, @DateTo, @DateFrom, (SELECT Building FROM Rooms WHERE Building = @Building AND FloorNr = @FloorNr AND Nr = @Nr), (SELECT FloorNr FROM Rooms WHERE Building = @Building AND FloorNr = @FloorNr AND Nr = @Nr), (SELECT Nr FROM Rooms WHERE Building = @Building AND FloorNr = @FloorNr AND Nr = @Nr), (SELECT Username FROM Users WHERE Username = @Username))
+END
+

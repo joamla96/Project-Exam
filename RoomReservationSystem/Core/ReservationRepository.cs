f06201b4d2c2a985d1c3c3e65fb0def287235c23
Matthew Peterson <matt2694@edu.eal.dk>
2017-05-18 16:14:11 +0200

    commented out a broken part

--- RoomReservationSystem/Core/ReservationRepository.cs
+++ RoomReservationSystem/Core/ReservationRepository.cs
@@ -74,7 +74,7 @@ namespace Core
             _reservationRepository.Add(reservation);
             reservation.Room.AddReservation(reservation);
             reservation.User.AddReservation(reservation);
-            _dalFacade.PassReservationToDAL(reservation);
+            //_dalFacade.PassReservationToDAL(reservation);
         }
 
         public void Delete(Reservation reservation)

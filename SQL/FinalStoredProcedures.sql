d99a58b302b2259ab624481adc4622c3f4809b4a
Jonas Laursen <jona8690@edu.eal.dk>
2017-05-18 10:23:34 +0200

    reformat for readability

--- SQL/FinalStoredProcedures.sql
+++ SQL/FinalStoredProcedures.sql
@@ -1,53 +1,58 @@
 CREATE PROCEDURE SP_GetAllUsers AS
 BEGIN
-SELECT Username, Email, PermissionLevel
-FROM Users
+	SELECT Username, Email, PermissionLevel
+	FROM Users
 END
 
 CREATE PROCEDURE SP_GetAllRooms AS
 BEGIN
-SELECT Building, FloorNr, Nr, MaxPeople, MinPermissionLevel
-FROM Rooms
+	SELECT Building, FloorNr, Nr, MaxPeople, MinPermissionLevel
+	FROM Rooms
 END
 
 CREATE PROCEDURE SP_GetAllReservations AS
 BEGIN
-SELECT ID, PeopleNr, DateTo, DateFrom, Building, FloorNr, Nr, Username
-FROM Reservations
+	SELECT ID, PeopleNr, DateTo, DateFrom, Building, FloorNr, Nr, Username
+	FROM Reservations
 END
 
-CREATE PROCEDURE SP_InsertUser (@Username NVarChar(100),
-								@Email NVarChar(MAX),
-								@PermissionLevel Int) AS
+CREATE PROCEDURE SP_InsertUser (
+	@Username NVarChar(100),
+	@Email NVarChar(MAX),
+	@PermissionLevel Int
+) AS
 BEGIN
 INSERT INTO Users (Username, Email, PermissionLevel) VALUES
-				  (@Username, @Email, @PermissionLevel)
-UPDATE Change set Identifier = 1 WHERE PrimaryKey = @Username
+	(@Username, @Email, @PermissionLevel)
+	UPDATE Change set Identifier = 1 WHERE PrimaryKey = @Username
 END
 
-CREATE PROCEDURE SP_InsertRoom (@Building Char,
-								@FloorNr NVarChar(max),
-								@Nr NVarChar(max),
-								@MaxPeople Int,
-								@MinPermissionLevel Int) AS
+CREATE PROCEDURE SP_InsertRoom (
+	@Building Char,
+	@FloorNr NVarChar(max),
+	@Nr NVarChar(max),
+	@MaxPeople Int,
+	@MinPermissionLevel Int
+) AS
 BEGIN
 INSERT INTO Rooms (Building, FloorNr, Nr, MaxPeople, MinPermissionLevel) VALUES
-				  (@Building, @FloorNr, @Nr, @MaxPeople, @MinPermissionLevel)
-UPDATE Change set Identifier = 1 WHERE PrimaryKey = @Building +';' + @FloorNr + ';' + @Nr
+	(@Building, @FloorNr, @Nr, @MaxPeople, @MinPermissionLevel)
+	UPDATE Change set Identifier = 1 WHERE PrimaryKey = @Building +';' + @FloorNr + ';' + @Nr
 END
 
-CREATE PROCEDURE SP_InsertReservation (@PeopleNr Int,
-								       @DateTo DateTime2,
-								       @DateFrom DateTime2,
-									   @Building Char,
-									   @FloorNr Int,
-									   @Nr Int,
-									   @Username NVarChar(100)) AS
+CREATE PROCEDURE SP_InsertReservation (
+	@PeopleNr Int,
+	@DateTo DateTime2,
+	@DateFrom DateTime2,
+	@Building Char,
+	@FloorNr Int,
+	@Nr Int,
+	@Username NVarChar(100)
+) AS
 BEGIN
-INSERT INTO Reservations (PeopleNr, DateTo, DateFrom, Building, FloorNr, Nr, Username) VALUES
---(@PeopleNr, @DateTo, @DateFrom, (SELECT Building FROM Rooms WHERE Building = @Building AND FloorNr = @FloorNr AND Nr = @Nr), (SELECT FloorNr FROM Rooms WHERE Building = @Building AND FloorNr = @FloorNr AND Nr = @Nr), (SELECT Nr FROM Rooms WHERE Building = @Building AND FloorNr = @FloorNr AND Nr = @Nr), (SELECT Username FROM Users WHERE Username = @Username))
-(@PeopleNr, @DateTo, @DateFrom, @Building, @FloorNr, @Nr, @Username)
-UPDATE Change set Identifier = 1 WHERE PrimaryKey = CAST(SCOPE_IDENTITY() AS NVarChar(100))
+	INSERT INTO Reservations (PeopleNr, DateTo, DateFrom, Building, FloorNr, Nr, Username) VALUES
+	(@PeopleNr, @DateTo, @DateFrom, @Building, @FloorNr, @Nr, @Username)
+	UPDATE Change set Identifier = 1 WHERE PrimaryKey = CAST(SCOPE_IDENTITY() AS NVarChar(100))
 END
 
 CREATE PROCEDURE SP_DeleteReservation (@ID Int) AS

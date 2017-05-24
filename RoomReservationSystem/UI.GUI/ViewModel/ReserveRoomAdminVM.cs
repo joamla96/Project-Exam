db1c47cc4a84558d701764284f3319d56ba13972
Jonas Laursen <jona8690@edu.eal.dk>
2017-05-24 13:48:28 +0200

    reserve room admin done

--- RoomReservationSystem/UI.GUI/ViewModel/ReserveRoomAdminVM.cs
+++ RoomReservationSystem/UI.GUI/ViewModel/ReserveRoomAdminVM.cs
@@ -11,6 +11,7 @@ namespace UI.GUI.ViewModel
 	class ReserveRoomAdminVM
 	{
 		ReservationRepository _repoReservation = ReservationRepository.Instance;
+		UserRepository _repoUser = UserRepository.Instance;
 
 		internal List<IRoom> GetAvailableRooms(string from, string to, string date)
 		{
@@ -22,5 +23,17 @@ namespace UI.GUI.ViewModel
 			return _repoReservation.GetAvailableRooms(dateFrom, dateTo, LoggedIn.User);
 		}
 		
+		internal void ReserveRoom(string date, string from, string to, IRoom room, string username)
+		{
+			IUser dummyUser = new User(username, "", Permission.Student);
+			IUser user = _repoUser.Get(dummyUser);
+
+			string dateTimeFrom = date + " " + from;
+			DateTime dateFrom = Convert.ToDateTime(dateTimeFrom);
+			string dateTimeTo = date + " " + to;
+			DateTime dateTo = Convert.ToDateTime(dateTimeTo);
+
+			_repoReservation.Add(user, room, 0, dateFrom, dateTo);
+		}
 	}
 }

b039c5ced077058d2fa9031ce54681a15e341283
Roxana Ion <ionroxana95@yahoo.com>
2017-05-26 10:42:19 +0200

    Monitor for database connection

--- RoomReservationSystem/DAL/Database.cs
+++ RoomReservationSystem/DAL/Database.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.Threading;
 
 namespace DAL {
 	public abstract class Database {
 		private static string _connInfo = DatabaseConn.ConnString;
 		private static SqlConnection _conn;
+		protected object locked;
 
 		protected SqlConnection OpenConnection() {
+			Monitor.Enter(locked);	
 			if (_conn == null) {
 				_conn = new SqlConnection(_connInfo);
 			}
@@ -17,6 +20,7 @@ namespace DAL {
 
 		protected void CloseConnection() {
 			_conn.Close();
+			Monitor.Exit(locked);
 		}
 	}
 }
